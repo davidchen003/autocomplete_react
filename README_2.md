3b721bcb253b61b299281e5c7ecd5c132755cfcb
David Chen <davidchen003@yahoo.com>
2021-12-24 18:31:55 -0800

    2 - display matched suggestions

---
+++ README_2.md
@@ -0,0 +1,31 @@
+[Course video](https://www.youtube.com/watch?v=Q2aky3eeO40)
+
+## Setup
+
+- `$ npx create-react-app autocomplete_react`
+- copy [admin-lte](https://cdnjs.com/libraries/admin-lte) CSS and paste the link in `public/index.html`
+
+  - `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/admin-lte/3.2.0-rc/css/adminlte.min.css" integrity="sha512-YOsl4pnOb5NC868yn1JxAzjJsWkLNtP53uc3OcyAl0Q2R1cwo/mdI1hHSQM8gbIxWj97mKeLoD9R0aiYibFQAA==" crossorigin="anonymous" referrerpolicy="no-referrer" />`
+
+- `src/App.js`, replace existing return content with `<input type="text" />`
+
+## Start server
+
+- `npm start`
+
+## Axios
+
+- so we can load (fake) data
+- `$ npm install axios`
+
+## src/App.js
+
+- `<div className="container">`: 'container' defined in admin-lte
+
+**Commit 1**
+
+## src/App.js
+
+- further development
+
+**Commit 2**
--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ function App() {
 
   const [users, setUsers] = useState([])
   const [text, setText] = useState('')
+  const [suggestions, setSuggests] = useState([])
+
   useEffect(() => {
   const loadUsers = async() => {
     const response = await axios.get('https://reqres.in/api/users')
@@ -17,7 +19,17 @@ function App() {
 }, [])
 
   const onChangeHandler = (text) => {
+    let matches = []
+    if (text.length > 0) {
+      matches = users.filter(user => {
+        const regex = new RegExp(`${text}`, "gi") //"gi": case insensitive
+        return user.email.match(regex)
+      })
+    }
+    //console.log('matches: ', matches)
+    setSuggests(matches)
     setText(text)
+
   }
 
   return (
@@ -27,6 +39,9 @@ function App() {
         onChange = {e => onChangeHandler(e.target.value)}
         value = {text}
         />
+      {suggestions && suggestions.map((suggestion,i) => 
+        <div key = {i}>{suggestion.email}</div>
+      )}
     </div>
   );
 }
