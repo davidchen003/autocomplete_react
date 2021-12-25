3b85375186cd8d218998fc804ca3c0b2d6a41583
David Chen <davidchen003@yahoo.com>
2021-12-24 17:42:24 -0800

    1 - show the text entered

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import axios from 'axios';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([])
+  const [text, setText] = useState('')
+  useEffect(() => {
+  const loadUsers = async() => {
+    const response = await axios.get('https://reqres.in/api/users')
+    //console.log(response.data)
+    setUsers(response.data.data)
+  }
+  loadUsers();
+}, [])
+
+  const onChangeHandler = (text) => {
+    setText(text)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"> 
+      <div>This is the text you entered: {text}</div>
+      <input type="text" className = "col-md-12" style= {{marginTop: 10}}
+        onChange = {e => onChangeHandler(e.target.value)}
+        value = {text}
+        />
     </div>
   );
 }
